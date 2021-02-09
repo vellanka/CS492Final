b7f5462fc58dbe03397091a64497ee45c57637af
Rob Hess <robwhess@gmail.com>
2021-02-09 14:56:57 -0800

    Add code from 2/9 lecture, part 1.

--- app/src/main/java/com/example/android/lifecycleawaregithubsearch/MainActivity.java
+++ app/src/main/java/com/example/android/lifecycleawaregithubsearch/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements GitHubSearchAdapt
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -64,6 +65,36 @@ public class MainActivity extends AppCompatActivity implements GitHubSearchAdapt
                 }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy()");
+        super.onDestroy();
     }
 
     private void doGitHubSearch(String query) {
